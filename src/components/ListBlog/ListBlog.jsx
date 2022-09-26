25e9d2b91664759151443ce3fbffc22462112556
Ajarek <ajarek@poczta.onet.pl>
2022-09-26 23:22:05 +0200

    fix handleToggle

--- src/components/ListBlog/ListBlog.jsx
+++ src/components/ListBlog/ListBlog.jsx
@@ -4,36 +4,28 @@ import classes from "./styles.module.css"
 
 export const ListBlog = (props) => {
   const { className, data, ...otherProps } = props
-  
+
   const [show, setShow] = useState(true)
- 
-  const  handleToggle = (e) => {
-    
-     
-    show?e.target.parentElement.parentElement.parentElement.className=`${classes.active}`
-    :e.target.parentElement.parentElement.parentElement.className=`${classes.nonactive}`
-    
-    setShow(show=>!show)
-    }
-    
-  
-  
+
+  const handleToggle = (e) => {
+    show
+      ? (e.target.parentElement.parentElement.parentElement.className = `${classes.active}`)
+      : (e.target.parentElement.parentElement.parentElement.className = `${classes.nonactive}`)
+
+    setShow((show) => !show)
+  }
+
   return (
     <>
       {data &&
         data.map((el) => (
-          
           <div
             key={el.id}
             id={el.id}
             className={`${classes.root}${className ? ` ${className}` : ""}`}
-           
             {...otherProps}
           >
-           
-            <div className={classes.wrapper}
-             
-            >
+            <div className={classes.wrapper}>
               <div className={classes.wrapper1}>
                 <img
                   className={classes.img}
@@ -41,26 +33,23 @@ export const ListBlog = (props) => {
                   alt="user"
                 />
               </div>
-              <div className={classes.wrapper2}
-              
-              >
+              <div className={classes.wrapper2}>
                 <div className={classes.wrapper3}>
                   <div className={classes.name}>{el.name}</div>
                   <div className={classes.text}>{el.text}</div>
                 </div>
-                <div className={classes.date}>{el.date} 
-                <button 
-                className={classes.btn}
-                 onClick={handleToggle}>{show?"Read":"Close"}
-                 </button>
+                <div className={classes.date}>
+                  {el.date}
+                  <button
+                    className={classes.btn}
+                    onClick={handleToggle}
+                  >
+                    Read
+                  </button>
                 </div>
               </div>
             </div>
-            <div  className={`${classes.message}`}
-           
-            >
-              {el.message}
-            </div>
+            <div className={`${classes.message}`}>{el.message}</div>
           </div>
         ))}
     </>
